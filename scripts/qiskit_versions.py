f26a3d7437b54b4afaa3eda9d3ca32aec76c8e4c
Karla Spuldaro <karla.spuldaro@ibm.com>
2023-05-15 17:30:35 -0400

    Use latest version in patch releases

--- scripts/qiskit_versions.py
+++ scripts/qiskit_versions.py
@@ -1,56 +1,71 @@
 import requests
 import json
-from datetime import datetime, timedelta
+from datetime import datetime
 
 def get_qiskit_versions_info() -> []:
     response = requests.get("https://pypi.org/pypi/qiskit-terra/json")
     data = response.json()
     data_items = data["releases"].items()
 
-    # List of objects containing relevant info of each package release, such as:
-    # version, date, python version
-    filtered_releases = []
+    # Release info temporary control dict.
+    temp = {}
 
     # Filter data
     for release, release_info in data_items:
+        # Skip RCs
+        if "rc" in release:
+            continue
+
+        # Filter releases starting from 2020-03
         date_str = release_info[0]["upload_time"]
+        date_format = "%Y-%m-%dT%H:%M:%S"
+        date_time_obj = datetime.strptime(date_str, date_format)
+        year = date_time_obj.year
+        month = date_time_obj.month
+
+        if (year == 2020 and month < 3) or year < 2020:
+            continue
+
         python_version = release_info[0]["requires_python"]
 
-        # Skip RCs and patch versions
-        if "rc" not in release:
-            # Skip patch versions
-            patch_number = int(release.split(".")[2])
-            patch = patch_number > 0
-            if not patch:
-                filtered_release_info = {"version":release, "date": date_str, "python_version": python_version}
+        # Parse the release string of format "x.y.z" into a list of "x","y","z"
+        dot_char = "."
+        major_minor_patch_list = release.split(dot_char)
+        major_minor = dot_char.join(major_minor_patch_list[:2])
 
-                # Filter releases starting from 2020-03
-                date_format = "%Y-%m-%dT%H:%M:%S"
-                date_time_obj = datetime.strptime(date_str, date_format)
-                year = date_time_obj.year
-                month = date_time_obj.month
+        # Get latest patch
+        patch_number = int(major_minor_patch_list[2])
+        temp_info = temp.get(major_minor)
+        previous_patch_number = -1 if not temp_info else temp_info[0]
 
-                if year == 2020 and month > 3 or year > 2020:
-                    filtered_releases.append(filtered_release_info)
+        if previous_patch_number < patch_number:
+            # Replace to latest patch version found
+            temp[major_minor] = (patch_number, {"version":release, "date": date_str, "python_version": python_version})
+            continue
 
-    # print('Qiskit Terra versions:')
-    # print(*filtered_releases, sep='\n')
+    filtered_releases = []
+    for _, value in temp.items():
+        filtered_releases.append(value[1])
+
     return filtered_releases
 
-"""
-Filtered data output example
+# Uncomment lines alone to test this file output on its own
+# versions_info = get_qiskit_versions_info()
+# print('Qiskit Terra versions:')
+# print(*versions_info, sep='\n')
 
-$ python scripts/qiskit_versions.py
-Qiskit Terra versions
-{'version': '0.13.0', 'date': '2020-04-09T21:22:39', 'requires_python': '>=3.5', 'python_version': 'cp35'}
-{'version': '0.14.0', 'date': '2020-04-30T20:52:56', 'requires_python': '>=3.5', 'python_version': 'cp35'}
-{'version': '0.15.0', 'date': '2020-08-06T18:02:45', 'requires_python': '>=3.5', 'python_version': 'cp35'}
-{'version': '0.16.0', 'date': '2020-10-15T21:31:06', 'requires_python': '>=3.6', 'python_version': 'cp36'}
-{'version': '0.17.0', 'date': '2021-04-01T15:51:06', 'requires_python': '>=3.6', 'python_version': 'cp36'}
-{'version': '0.18.0', 'date': '2021-07-12T19:41:19', 'requires_python': '>=3.6', 'python_version': 'cp36'}
-{'version': '0.19.0', 'date': '2021-12-06T20:03:48', 'requires_python': '>=3.6', 'python_version': 'cp36'}
-{'version': '0.20.0', 'date': '2022-03-31T21:05:16', 'requires_python': '>=3.7', 'python_version': 'cp310'}
-{'version': '0.21.0', 'date': '2022-06-30T17:16:08', 'requires_python': '>=3.7', 'python_version': 'cp310'}
-{'version': '0.22.0', 'date': '2022-10-13T19:23:19', 'requires_python': '>=3.7', 'python_version': 'cp310'}
-{'version': '0.23.0', 'date': '2023-01-26T22:07:35', 'requires_python': '>=3.7', 'python_version': 'cp310'}
+"""
+Qiskit Terra versions:
+{'version': '0.13.0', 'date': '2020-04-09T21:22:39', 'python_version': '>=3.5'}
+{'version': '0.14.2', 'date': '2020-06-15T21:02:04', 'python_version': '>=3.5'}
+{'version': '0.15.2', 'date': '2020-09-08T18:01:48', 'python_version': '>=3.5'}
+{'version': '0.16.4', 'date': '2021-02-08T17:16:48', 'python_version': '>=3.6'}
+{'version': '0.17.4', 'date': '2021-05-18T21:24:27', 'python_version': '>=3.6'}
+{'version': '0.18.3', 'date': '2021-09-29T11:57:07', 'python_version': '>=3.6'}
+{'version': '0.19.2', 'date': '2022-02-02T14:03:42', 'python_version': '>=3.6'}
+{'version': '0.20.2', 'date': '2022-05-18T15:29:49', 'python_version': '>=3.7'}
+{'version': '0.21.2', 'date': '2022-08-23T21:07:29', 'python_version': '>=3.7'}
+{'version': '0.22.4', 'date': '2023-01-17T13:01:47', 'python_version': '>=3.7'}
+{'version': '0.23.3', 'date': '2023-03-21T19:16:16', 'python_version': '>=3.7'}
+{'version': '0.24.0', 'date': '2023-05-04T19:14:05', 'python_version': '>=3.7'}
 """
